f87e560dc7a0110be8c3e143533019554a84f3d2
IsamarDavilaG <isamar.dvgz@gmail.com>
2016-12-23 12:20:35 -0500

    index

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
-function monoB()
-{
-	var img = document.getElementById("img_mono_b");
-	var checkB = document.getElementById("mono_b");
-
-	if(checkB.checked)
-		img.innerHTML = '<img src="img/feliz.jpg">';
-	else
-		img.innerHTML = '<img src="img/triste.jpg">';
-}
-function monoA()
-{
-	var img = document.getElementById("img_mono_a");
-	var checkA = document.getElementById("mono_a");
-
-	if(checkA.checked)
-	{
-		img.innerHTML = '<img class="imagen" src="img/feliz.jpg">';
-	}else{
-		img.innerHTML = '<img src="img/triste.jpg">';
+function calcular(){
+	var n=document.getElementById("numero").value;
+	var diff;
+	var salida =document.getElementById("salida");
+	if( n>21){
+		diff=n-21;
+		diff=diff*2;
+		salida.innerHTML=diff;
 	}
-}
-
-function evaluar()
-{
-	var aSmile = document.getElementById("mono_a").checked;
-	var bSmile = document.getElementById("mono_b").checked;
-	var salida = document.getElementById("salida");
-
-	if(aSmile == bSmile)
-	{
-		//problemas
-		salida.innerHTML = "1";
-	}else{
-		//NO hay problema
-		salida.innerHTML = "0";
+	else{
+		diff=21-n;
+		salida.innerHTML=diff;
 	}
 }
